ff7d5e10c0553d158f0b2f1f79fd2aa1a8f14ef8
taleshrocha <taleshrochaz@gmail.com>
2023-02-13 14:22:14 -0300

    CheckBox

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,8 +1,8 @@
 import { NewsContext } from "@/contexts/NewsContext";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 
-export default function Table({data}) {
-  const {trustThreshold} = useContext(NewsContext)
+export default function Table({ data }) {
+  const { trustThreshold } = useContext(NewsContext);
   return (
     <div className="overflow-y-scroll rounded-lg border-4 border-gray-800 h-full">
       <table className="table-fixed text-center w-full h-full text-white">
@@ -12,13 +12,14 @@ export default function Table({data}) {
             <td>Content</td>
             <td>Fake rate</td>
             <td>State</td>
+            <td>-</td>
           </tr>
         </thead>
         <tbody>
           {data.map((news, index) => (
             <tr
               key={news.id}
-              className="hover:bg-gray-600 hover:cursor-pointer"
+              className="hover:bg-gray-600"
               onClick={() => console.log(news)}
             >
               <td>{index + 1}</td>
@@ -26,13 +27,17 @@ export default function Table({data}) {
               <td className="">{news.fakeRate}</td>
               <td
                 className={`
-                  font-bold ${news.fakeRate * 100 < trustThreshold
-                    ? "text-green-500"
-                    : "text-red-500"
+                  font-bold ${
+                    news.fakeRate * 100 < trustThreshold
+                      ? "text-green-500"
+                      : "text-red-500"
                   }
                 `}
               >
                 {news.fakeRate * 100 < trustThreshold ? "True" : "Fake"}
+              </td>
+              <td className="table-cell">
+                <CheckBox marked={false} />
               </td>
             </tr>
           ))}
@@ -40,4 +45,13 @@ export default function Table({data}) {
       </table>
     </div>
   );
+}
+
+function CheckBox({ marked }) {
+  const [isMarked, setIsMarked] = useState(marked);
+
+  return <button 
+    className="text-center h-5 w-5 bg-white text-black"
+    onClick={() => setIsMarked(!isMarked)}
+  >{isMarked? "X" : "."}</button>;
 }
