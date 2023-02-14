bb7640ec0c80676ebfcae52b3f276bd8e0e5d886
taleshrocha <taleshrochaz@gmail.com>
2023-02-14 10:10:27 -0300

    CheckBoxes working

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,29 +1,45 @@
 import { NewsContext } from "@/contexts/NewsContext";
-import { allNews } from "@/pages/data";
-import { useContext, useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 
 export default function Table({ data }) {
   const { trustThreshold } = useContext(NewsContext);
+  const [allChecked, setAllChecked] = useState(false);
+  const [rateMed, setRateMed] = useState(0);
+  const [trueNews, setTrueNews] = useState(0);
+  const [checkBoxValue, setCheckBoxValue] = useState(
+    Array(Object.keys(data).length).fill(false)
+  );
+  const checkRef = useRef(null);
+
+  function allAreTrue(arr) {
+    return arr.every((element) => element === true);
+  }
 
   return (
-    <div className="flex flex-col overflow-y-scroll rounded-lg border-4 border-gray-800 h-full">
+    <div className="relative flex flex-col overflow-y-scroll rounded-lg border-4 border-gray-800 h-full">
       <table className="table-fixed text-center w-full h-full text-white">
-        <thead>
+        <thead className="sticky top-0">
           <tr className="bg-gray-800">
             <td>-</td>
             <td>Content</td>
             <td>Fake rate</td>
             <td>State</td>
-            <td>-</td>
+            <td>
+              <input
+                type="checkbox"
+                onChange={(e) => {
+                  setCheckBoxValue(
+                    Array(Object.keys(data).length).fill(e.target.checked)
+                  );
+                }}
+                ref={checkRef}
+              />
+            </td>
           </tr>
         </thead>
         <tbody>
           {data.map((news, index) => (
-            <tr
-              key={news.id}
-              className="hover:bg-gray-600"
-              onClick={() => console.log(news)}
-            >
+            <tr key={news.id} className="hover:bg-gray-600">
               <td>{index + 1}</td>
               <td className="truncate">{news.content}</td>
               <td className="">{news.fakeRate}</td>
@@ -38,29 +54,23 @@ export default function Table({ data }) {
               >
                 {news.fakeRate * 100 < trustThreshold ? "True" : "Fake"}
               </td>
-              <td className="">
-                  <input
-                    type="checkbox"
-                    className=""
-                  />
+              <td>
+                <input
+                  type="checkbox"
+                  onChange={(e) => {
+                    let newValue = [...checkBoxValue];
+                    newValue[index] = e.target.checked;
+                    checkRef.current.checked = allAreTrue(newValue)
+                    setCheckBoxValue(newValue);
+                  }}
+                  checked={checkBoxValue[index]}
+                  className=""
+                />
               </td>
             </tr>
           ))}
         </tbody>
       </table>
     </div>
-  );
-}
-
-function CheckBox({ marked }) {
-  const [isMarked, setIsMarked] = useState(marked);
-
-  return (
-    <button
-      className="text-center h-5 w-5 bg-white text-black"
-      onClick={() => setIsMarked(!isMarked)}
-    >
-      {isMarked ? "X" : "."}
-    </button>
   );
 }
